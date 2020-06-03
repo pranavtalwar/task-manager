dbe2b64ac53221524dc83269086415e3d02c1835
Pranav Talwar <talwar.pranav123@gmail.com>
2020-06-04 00:11:19 +0530

    added delete functionality for resource

--- src/index.js
+++ src/index.js
@@ -64,6 +64,20 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const user = await User.findByIdAndDelete(_id)
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -120,6 +134,20 @@ app.patch('/tasks/:id', async (req, res) => {
         res.status(400).send()
     }
 
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findByIdAndDelete(_id)
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch(e) {
+        res.status(500).send()
+    }
 })
 
 app.listen(port, () => {
