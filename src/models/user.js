97b4f0ba1bc80b11e2644a312795686d2a1ed05e
Pranav Talwar <talwar.pranav123@gmail.com>
2020-06-04 01:00:22 +0530

    added login functionality for user

--- src/models/user.js
+++ src/models/user.js
@@ -13,6 +13,7 @@ const userSchema = new mongoose.Schema({
         required: true,
         trim: true,
         lowercase: true,
+        unique: true,
         validate(value) {
             if(!validator.isEmail(value)) {
                 throw new Error('Invalid email')
@@ -41,6 +42,23 @@ const userSchema = new mongoose.Schema({
     }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if(!user) {
+        throw new Error('Unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+}
+
+// Hash the plain text password before saving 
 userSchema.pre('save', async function (next) {
     const user = this
 
