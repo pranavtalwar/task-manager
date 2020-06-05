3036230bc0dfb5f7985f14fc8179a3fffa803af8
Pranav Talwar <talwar.pranav123@gmail.com>
2020-06-05 18:07:44 +0530

    added sorting option to api

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,15 +19,21 @@ router.post('/tasks', auth, async (req, res) => {
 
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
-
+    const sort = {}
     if(req.query.completed) {
         match.completed = req.query.completed === 'true'
+    }
+
+    if(req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
 
     try {
         const tasks = await Task.find({ owner: req.user._id, ...match })
         .limit(parseInt(req.query.limit))
         .skip(parseInt(req.query.skip))
+        .sort(sort)
         res.send(tasks)
     } catch(e) {
         console.log(e)
