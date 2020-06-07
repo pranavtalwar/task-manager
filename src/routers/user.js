068ee3c2fbea0f42b59e587dc1193a496ad9029c
Pranav Talwar <talwar.pranav123@gmail.com>
2020-06-07 17:51:17 +0530

    added mocks and more user tests

--- src/routers/user.js
+++ src/routers/user.js
@@ -61,7 +61,7 @@ router.patch('/users/me', auth, async (req, res) => {
     const isValidOperation = updates.every((update) =>  allowedUpdates.includes(update))
 
     if(!isValidOperation) {
-        return res.status(404).send({ error : 'Invalid updates' })
+        return res.status(400).send({ error : 'Invalid updates' })
     }
 
     try {
--- tests/user.test.js
+++ tests/user.test.js
@@ -87,7 +87,7 @@ test('Should delete account for user', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
-
+    
     const user = await User.findById(userOneId)
     expect(user).toBeNull()
 })
@@ -97,4 +97,39 @@ test('Should not delete account for unauthenticated user', async () => {
         .delete('/users/me')
         .send()
         .expect(401)
+})
+
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar','tests/fixtures/profile-pic.jpg')
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async() => {
+    await request(app)
+        .patch('/users/me')
+        .send({
+            name: 'Andy'
+        })
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .expect(200)
+
+    const user = await User.findById(userOneId)
+    expect(user.name).toBe('Andy')
+})
+
+test('Shoul not update invalid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .send({
+            location: 'India'
+        })
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .expect(400)
+
 })
